d3cd15dd97127d8b240aface4ba3bb7566fd28cd
NDC <79379545+NDCsGitHub@users.noreply.github.com>
2022-06-09 21:56:42 -0400

    ss

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,82 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+
 
 function Login() {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+    const { email, password } = formData
+
+
+
+
+    const handleOnChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+
+
+    }
+
     return (
-        <div>Login</div>
+
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaSignInAlt /> Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+
+
+            <section className='form'>
+                <form onSubmit={(e) => { onSubmit(e) }}>
+                    <div className='form-group'>
+
+                        <input
+                            type='email'
+                            className='form-control'
+                            name='email' id='email'
+                            value={email}
+                            placeholder='Email'
+                            onChange={(e) => {
+                                handleOnChange(e)
+                            }}
+                        />
+
+                        <input
+                            type='password'
+                            className='form-control'
+                            name='password' id='password'
+                            value={password}
+                            placeholder='password'
+                            onChange={(e) => {
+                                handleOnChange(e)
+                            }}
+                        />
+
+                    </div>
+
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block'>
+                            Submit
+                        </button>
+                    </div>
+
+                </form>
+            </section>
+        </>
+
     )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,114 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { FaUser } from 'react-icons/fa'
+
 
 function Register() {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+        admin: '',
+    })
+
+    const { name, email, password, password2, admin } = formData
+
+    const handleOnChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+
+
+    }
+
     return (
-        <div>Register</div>
+
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaUser /> Register
+                </h1>
+                <p>Please create an account</p>
+            </section>
+
+
+            <section className='form'>
+                <form onSubmit={(e) => { onSubmit(e) }}>
+                    <div className='form-group'>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='name' id='name'
+                            value={name}
+                            placeholder='Name'
+                            onChange={(e) => {
+                                handleOnChange(e)
+                            }}
+                        />
+
+                        <input
+                            type='email'
+                            className='form-control'
+                            name='email' id='email'
+                            value={email}
+                            placeholder='Email'
+                            onChange={(e) => {
+                                handleOnChange(e)
+                            }}
+                        />
+
+                        <input
+                            type='password'
+                            className='form-control'
+                            name='password' id='password'
+                            value={password}
+                            placeholder='password'
+                            onChange={(e) => {
+                                handleOnChange(e)
+                            }}
+                        />
+
+                        <input
+                            type='password'
+                            className='form-control'
+                            name='password2' id='password2'
+                            value={password2}
+                            placeholder='Confirm Password'
+                            onChange={(e) => {
+                                handleOnChange(e)
+                            }}
+                        />
+
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='admin' id='admin'
+                            value={admin}
+                            placeholder='Admin'
+                            onChange={(e) => {
+                                handleOnChange(e)
+                            }}
+                        />
+                    </div>
+
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block'>
+                            Submit
+                        </button>
+                    </div>
+
+                </form>
+            </section>
+        </>
+
     )
 }
 
